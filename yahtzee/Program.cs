5ac06b6bfd5f13ba9a00f85ed5bf13d816076337
Carlile Upfront <carlileupfront@users.noreply.github.com>
2019-03-02 14:51:13 -0600

    Add project files.

---
+++ yahtzee/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace yahtzee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
